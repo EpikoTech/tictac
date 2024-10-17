1fc24d159677c6a42ba07e519c6e60f0bbf7e589
RogerJackD <1511638@senati.pe>
2024-10-17 11:06:21 -0500

    spans

--- registro.js
+++ registro.js
@@ -1,5 +1,19 @@
 
 
-document.getElementById('registerForm').addEventListener('submit',()=>{
-    alert('lo enviaste')
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault()
+    let valid = true;
+
+    //usuario
+    const usuario = document.getElementById('usuario');
+    const usuarioError = document.getElementById('usuarioError');
+    if (usuario.value.trim() === "") {
+        usuarioError.style.display = 'block';
+        valid = false;
+    } else {
+        usuarioError.style.display = 'none';
+    }
+
+
+    
 })
